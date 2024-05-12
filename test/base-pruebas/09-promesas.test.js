425fc77cc4f3d57b15643059190606b926242e20
Ernesto <jorge.cywdt@gmail.com>
2024-05-12 16:55:18 -0500

    a

---
+++ test/base-pruebas/09-promesas.test.js
@@ -0,0 +1,14 @@
+import { getHeroeByIdAsync } from '../../src/base-pruebas/09-promesas';
+
+describe('Pruebas en 09-promesas', () => {
+
+    test('getHeroeByIdAsync debe de retornar un héroe', () => {
+
+        // 1. inicialización
+        // const
+
+        // 3. observar el comportamiento... esperado
+
+    });
+
+});
